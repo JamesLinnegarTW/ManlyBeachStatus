fc65619701e19705bef85883e3fa3f0f5965f7ad
James Linnegar <jlinnega@thoughtworks.com>
2014-11-19 20:53:16 +1100

    adding temperatures

--- tide.py
+++ tide.py
@@ -1,4 +1,5 @@
 import feedparser
+import RPi.GPIO as GPIO
 from Adafruit_CharLCD import Adafruit_CharLCD
 
 from time import sleep
@@ -10,13 +11,17 @@ def getData():
     data = feedparser.parse('http://www.environment.nsw.gov.au/beachapp/SydneyBulletin.xml')
     return data
 
-def setTides(data):
+def getTides(data):
     high_tide = data['channel']['bw_hightide'].replace(" metres at", "m ").strip(".")
     low_tide = data['channel']['bw_lowtide'].replace(" metres at", "m ").strip(".")
     return high_tide, low_tide
 
-def printBeach():
-    lcd.message('hello')
+
+def getTemperatures(data):
+    air_temperature =  data['channel']['bw_airtemp']
+    sea_temperature =  data['channel']['bw_oceantemp']
+    return air_temperature, sea_temperature
+
 
 def clear():
     lcd.clear()
@@ -27,14 +32,24 @@ def tides(high_tide, low_tide):
     lcd.message("H: %s" % high_tide)
     lcd.setCursor(0,1)
     lcd.message("L: %s" % low_tide)
-    
+
+def temperature(air_temp, sea_temp)
+    clear()
+    lcd.setCursor(0,0)
+    lcd.message("Air: %sc" % air_temp)
+    lcd.setCursor(0,1)
+    lcd.message("Sea: %sc" % sea_temp)
+
 while 1:
     beach_data = getData()
-    high_tide, low_tide = setTides(beach_data)
+    high_tide, low_tide = getTides(beach_data)
+    air_temperature, sea_temperature = getTemperatures(beach_data);
+
     clear()
     for beach in beach_data.entries:
        if(beach.title == "Little Manly Cove"):
-           tides(high_tide, low_tide)
+#           tides(high_tide, low_tide)
+           temperature(air_temperature, sea_temperature)
     sleep(5)
 
 
